0074b30173922897641bd960821f8cc1d1aa629d
Stefane Fivaz <31249205+sfivaz@users.noreply.github.com>
2019-11-17 22:17:58 +0100

    1

---
+++ index.php
@@ -0,0 +1,65 @@
+
+
+<html>
+
+<body>
+
+<form>
+    <div>
+        <label>marque ou modele</label>
+        <input>
+    </div>
+
+    <div>
+        <label>année</label>
+        <input>
+    </div>
+
+    <div>
+        <label>uniquement modeles anciens</label>
+        <input type="checkbox">
+    </div>
+
+    <div>
+        <label>impôt max</label>
+        <input>
+    </div>
+
+    <div>
+        <label>assurance max</label>
+        <input>
+    </div>
+
+
+    <div>
+        <p>tri</p>
+        <label>modèle</label>
+        <input type="radio">
+
+        <label>année</label>
+        <input type="radio">
+
+        <label>coût</label>
+        <input type="radio">
+
+    </div>
+
+    <div>
+        <select>
+            <option selected="selected">HTML</option>
+            <option>PDF</option>
+        </select>
+    </div>
+
+    <div>
+        <button type="submit">rechercher</button>
+    </div>
+</form>
+
+<div>
+
+</div>
+
+</body>
+
+</html>
